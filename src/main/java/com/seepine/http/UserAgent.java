30de97cea70d204b194ea1127d79a2f1fdea7381
seepine <seepine@outlook.com>
2023-03-11 14:55:09 +0800

    improvement(ua): add common userAgent

---
+++ src/main/java/com/seepine/http/UserAgent.java
@@ -0,0 +1,16 @@
+package com.seepine.http;
+
+/**
+ * @author seepine
+ * @since 1.0.1
+ */
+public interface UserAgent {
+  String WIN10 =
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+  String IPHONE =
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/110.0.0.0";
+  String IPAD =
+      "Mozilla/5.0 (iPad; CPU OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1 Edg/110.0.0.0";
+  String ANDROID =
+      "Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36 Edg/110.0.0.0";
+}
